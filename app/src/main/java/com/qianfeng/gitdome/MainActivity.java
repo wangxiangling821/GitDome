40fc540b76d6280f8bf65a42d6e31afaf3bef6ef
Administrator <1305521500@qq.com>
2016-04-29 17:03:39 +0800

    第四次clone!

--- app/src/main/java/com/qianfeng/gitdome/MainActivity.java
+++ app/src/main/java/com/qianfeng/gitdome/MainActivity.java
@@ -2,14 +2,18 @@ package com.qianfeng.gitdome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity{
 private int a;
+    private View byId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         a = 45;
         a= 345;
+        byId = findViewById(R.id.tv_show);
     }
 }
